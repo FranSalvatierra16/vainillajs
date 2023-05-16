812b5fee4ff45cb6317d01970a221a422955b032
FranSalvatierra16 <103911827+FranSalvatierra16@users.noreply.github.com>
2023-05-16 23:52:56 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  let randomNumber = Math.random() * 10;
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
+};
+let generateExcuse = () => {
+  let pronoun = ["A", "The"];
+  let subject = ["jogger", "racoon", "dog", "driver", "comedion", "pincone"];
+  let action = ["homework", "toe", "car", "shoe"];
+  let possetion = ["took my", "threw my", "yelled at my", "stole my", "bit my"];
+  let where = ["on the street", "in my house", "in my driveway"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let proIndx = Math.floor(Math.random() * pronoun.length);
+  let subIndx = Math.floor(Math.random() * subject.length);
+  let actionIndx = Math.floor(Math.random() * action.length);
+  let possIndx = Math.floor(Math.random() * possetion.length);
+  let whereIndx = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[proIndx] +
+    " " +
+    subject[subIndx] +
+    " " +
+    action[actionIndx] +
+    " " +
+    possetion[possIndx] +
+    " " +
+    where[whereIndx] +
+    " "
+  );
 };
